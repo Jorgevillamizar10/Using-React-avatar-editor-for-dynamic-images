2c640e5d656169ee1cb8f2251109e2efe17a076e
Jorge Villamizar <jorgevillamizar14@gmail.com>
2020-11-24 21:49:55 -0400

    change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React, { useState } from "react";
-import { SvgComponent } from "./components/index";
+import { SvgComponent } from "./Components/index";
 import { Editor } from "./RecortarImagen/index";
 
 const App = () => {
-	const Ruta =
-		"https://i.pinimg.com/736x/3f/af/03/3faf0372cf1a41245b2fba14a604b07d.jpg";
-	const Ruta2 =
-		"https://static.iris.net.co/soho/upload/images/2020/2/10/61074_1.jpg";
-	const Ruta3 =
-		"https://i.pinimg.com/236x/86/b2/52/86b2521ebaa17c8a89f85609809bd9a6.jpg";
 
 	const [aux, setAux] = useState("");
 
